fbcb2381ea6b5cf48ca9201ef822563e9eecc839
Lakkimsetti Eswar Sai Kumar <eswarsaikumar2@gmail.com>
2025-04-23 09:06:46 +0530

    provider

--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -5,13 +5,13 @@ terraform {
       version = "5.48.0"
     }
   }
-  backend "s3" {
+  /*  backend "s3" {
     bucket         = "s3bucket-project-devops"
     key            = "expense-dev-sg"
     region         = "us-east-1"
     dynamodb_table = "use_lock"
-  }
-}
+  } */
+} 
 provider "aws" {
   region = "us-east-1"
 }
