f0bd2a10f85e74c102c77293adbdeb89719aab0e
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-04 15:26:24 +0530

    updated

---
+++ 01-vpc/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "s3bucket-project-devops"
+    key            = "expense-dev-vpc"
+    region         = "us-east-1"
+    dynamodb_table = "use_lock"
+  } 
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
