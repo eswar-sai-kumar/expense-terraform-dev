ca1bf28bd1b3185aaa4b2645842717f5b0f62213
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-06 08:25:00 +0530

    updated

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -9,7 +9,7 @@ terraform {
     bucket = "s3bucket-project-devops"
     key    = "expense-dev-vpc"
     region = "us-east-1"
-    dynamodb_table = "daws78s-locking"
+    dynamodb_table = "table-project-devops"
   }
 }
 
