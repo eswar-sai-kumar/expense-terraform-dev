22d7b8a70dd4b31b47430b5afa53400923b086a9
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-24 18:51:28 +0530

    update

---
+++ main.sh
@@ -0,0 +1,25 @@
+sudo su -
+cd /etc/yum.repos.d/
+
+CONTENT="
+[jenkins]
+name=Jenkins-stable
+baseurl=http://pkg.jenkins.io/redhat-stable
+gpgcheck=1
+"
+
+vim "jenkins.repo" <<EOF
+:1
+i
+$CONTENT
+.
+:wq
+EOF
+
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install fontconfig java-21-openjdk -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
+
