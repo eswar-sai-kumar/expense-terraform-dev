0555ba59504c31b1f77ce9f8de8c9c32087c0042
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-24 18:53:35 +0530

    update

--- jenkins-ec2-server/main.tf
+++ jenkins-ec2-server/main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "jenkins" {
   associate_public_ip_address = true
 }
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.jenkins.public_ip
 }
