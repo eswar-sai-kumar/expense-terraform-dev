f80a20dc1d7b868fe1a3a6e2e319dabc50bfc6dc
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-24 19:16:49 +0530

    update

--- main.sh
+++ main.sh
@@ -9,22 +9,21 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Step 1: Go to yum repo directory
 cd /etc/yum.repos.d/
 
-# Step 2: Download Jenkins repo file directly
-curl -o jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+cat <<EOF > jenkins.repo
+[jenkins]
+name=Jenkins-stable
+baseurl=http://pkg.jenkins.io/redhat-stable
+gpgcheck=1
+EOF
 
-# Step 3: Import Jenkins GPG key
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-# Step 4: Install Java (required for Jenkins)
 yum install -y fontconfig java-21-openjdk
 
-# Step 5: Install Jenkins
 yum install -y jenkins
 
-# Step 6: Reload systemd, enable and start Jenkins
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
